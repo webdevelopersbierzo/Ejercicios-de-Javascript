784f2b1084b863073efc2d4c8739643e5fc7cb19
webdevelopersbierzo <oscarcorralg@hotmail.com>
2024-11-09 07:49:37 +0100

    End Exercises_2

---
+++ Exercise_2/myscript.js
@@ -0,0 +1,7 @@
+let lado = prompt("Introduce la longitud del lado del cuadrado");
+
+let area = parseInt(lado) * parseInt(lado);
+let perimetro = parseInt(lado) + parseInt(lado) + parseInt(lado) + parseInt(lado);
+
+console.log("El area del cuadrado es: " + area)
+console.log("El perimetro del cuadrado es: " + perimetro);
