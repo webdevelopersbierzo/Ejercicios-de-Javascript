01c889428295cb147f394274ef06f9fcd8d31e10
webdevelopersbierzo <oscarcorralg@hotmail.com>
2024-11-09 17:59:22 +0100

    End exercise 8

---
+++ Exercise_8/myscript.js
@@ -0,0 +1,6 @@
+let comensales = prompt("Introduce el numero de comensales");
+let papas = comensales * 200 / 1000;
+let huevos = comensales *(200 * 5/ 1000);
+let cebollas = comensales *(200 * 300 / 1000);
+
+alert("Para " + comensales + " comensales" + " Se necesitan : " + huevos + " huevos " + papas + " kg papas " + cebollas +" cebollas " );
