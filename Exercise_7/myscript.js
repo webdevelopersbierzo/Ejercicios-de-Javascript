4aafef39a2da9102e94e5b42655c051443582b83
webdevelopersbierzo <oscarcorralg@hotmail.com>
2024-11-09 17:42:01 +0100

    End exercises 7

---
+++ Exercise_7/myscript.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Introduce un numero de dos cifras: "))
+
+let cociente = parseInt(numero / 10);
+let resto = numero % 10;
+
+alert( "las Decenas son : "  + cociente + " las unidades son " +  resto )
