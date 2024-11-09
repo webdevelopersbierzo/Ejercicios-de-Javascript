b4cb9d76a67d36ca4fd8aedf5d409dee29872da7
webdevelopersbierzo <oscarcorralg@hotmail.com>
2024-11-09 18:07:28 +0100

    End Exercise 9

---
+++ Exercise_9/myscript.js
@@ -0,0 +1,9 @@
+let uno = 10;
+let dos = 30;
+console.log("Valor de la variable uno " + uno);
+console.log("Valor de la variable dos " + dos);
+let aux = dos;
+dos= uno;
+uno = aux;
+console.log("Valor de la variable uno " + uno);
+console.log("Valor de la variable dos " + dos);
