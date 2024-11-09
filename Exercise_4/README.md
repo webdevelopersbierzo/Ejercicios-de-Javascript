f2ab8007a5b14dbd5d9adb4d37a4791289f3a810
webdevelopersbierzo <oscarcorralg@hotmail.com>
2024-11-09 08:10:20 +0100

    End Exercise 4

---
+++ Exercise_4/README.md
@@ -0,0 +1,3 @@
+## Exercise 4
+En este ejercicio vas a pedir al usuario que teclee tres numeros enteros y el script mostrara como resultado el  valor medio de los tres.
+Recuerda que la media de tres números se calcula sumando los tres y dividiendo entre 3
---
+++ Exercise_4/myscript.js
@@ -0,0 +1,6 @@
+let numberOne = parseInt(prompt("Introduce el primer numero"));
+let numberTwo = parseInt(prompt("Introduce el segundo numero"));
+let numberThree = parseInt(prompt("Introduce el tercer numero"));
+
+let resultado = (numberOne + numberTwo + numberThree) / 3 ;
+console.log(resultado);
